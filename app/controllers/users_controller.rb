7934a892054c205b837a73313e2fcf310e1ba890
Adam Aiken <adamaikenadam@gmail.com>
2017-12-03 10:31:58 +0800

    first draft completed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+
+  ITERATION = 10_000
+  def batch_create
+    require 'database_cleaner'
+    DatabaseCleaner.strategy = :truncation
+    cleanup_db
+    require 'faker'
+
+    # ### Not using activerecord import
+    no_ari_time = benchmarking {
+      ITERATION.times do |i|
+        create_random_user.save!
+      end
+    }
+
+    ## Not using activerecord import
+    ari_time = benchmarking {
+      users = []
+      ITERATION.times do |i|
+        users << create_random_user
+      end
+      User.import users, batch_size: 1_000    # or use import!
+    }
+
+    redirect_to root_path, notice: "No ARI: #{no_ari_time}, With ARI: #{ari_time}"
+   
+  end
+
+  private
+    def cleanup_db
+      # then, whenever you need to clean the DB
+      DatabaseCleaner.clean
+    
+    end
+
+    def mark_time
+      Time.now
+    end
+
+    def benchmarking
+      start_time = mark_time
+      
+      yield
+      end_time = mark_time
+      cleanup_db
+      measured_time = (end_time - start_time)
+    end
+
+    def create_random_user
+      random_count = ((1..100).to_a)
+      User.new(
+        name: Faker::Name.unique.name + random_count.sample.to_s,
+        email: Faker::Internet.email + random_count.sample.to_s,
+        age: random_count.sample,
+        job: Faker::Job.title,
+        company: Faker::Company.name,
+        )
+    end
+end
